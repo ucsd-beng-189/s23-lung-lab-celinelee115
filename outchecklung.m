93a85dc8b9c2157c333a9ad87f78edbf2eddfff1
celinelee115 <123041751+celinelee115@users.noreply.github.com>
2023-04-23 21:59:54 -0700

    outchecklung_task8.m

--- outchecklung.m
+++ outchecklung.m
@@ -4,65 +4,69 @@
 %solve for all other unknowns:
 %concentrations and partial pressures 
 %in the individual alveoli:
-%vector of oxygen concentrations in 
-%arterial blood leaving each alveolus:
-ca=carterial(cv,r);
-%
-%vector of oxygen partial pressures
-%in arterial blood leaving each alveolus:
-Pa=H(ca);
-%
-%vector of oxygen partial pressures 
-%in alveolar air:
-PA=Pa;
-%
-%vector of oxygen concentrations
-%in alveolar air:
-cA=PA/RT;
-%
-%concentrations and partial pressures 
-%for the whole organism:
-%(write out these results)
-%oxygen concentration in (systemic) venous blood:
-cv;
-%
-%mean arterial oxygen concentration:
-cabar=Q' *ca/sum(Q);
-%  
-%mean alveolar oxygen concentration:
-cAbar=VA'*cA/sum(VA);
-%
-%oxygen concentration is the inspired air:
-cI;
-%
-%oxygen partial pressure in venous blood:
-Pv=H(cv);
-%
-%mean arterial oxygen partial pressure:
-Pabar=H(cabar);
-%
-%mean alveolar oxygen partial pressure:
-PAbar=RT.*cAbar;
-%
-%oxygen partial pressure in the inspired air:
-PI=RT.*cI;
-% 
-
-% Get vector of altitude
-altitude=[0 5000 10000 15000 20000 25000];
+for i=1:length(beta)
+    %vector of oxygen concentrations in 
+    %arterial blood leaving each alveolus:
+    ca=carterial(cv_vec(i),r(:,i));
+    %
+    %vector of oxygen partial pressures
+    %in arterial blood leaving each alveolus:
+    Pa=H(ca);
+    %
+    %vector of oxygen partial pressures 
+    %in alveolar air:
+    PA=Pa;
+    %
+    %vector of oxygen concentrations
+    %in alveolar air:
+    cA=PA/RT;
+    %
+    %concentrations and partial pressures 
+    %for the whole organism:
+    %(write out these results)
+    %oxygen concentration in (systemic) venous blood:
+    cv;
+    %
+    %mean arterial oxygen concentration:
+    cabar=Q' *ca/sum(Q);
+    %  
+    %mean alveolar oxygen concentration:
+    cAbar=VA'*cA/sum(VA);
+    %
+    %oxygen concentration is the inspired air:
+    cI;
+    %
+    %oxygen partial pressure in venous blood:
+    Pv=H(cv);
+    %
+    %mean arterial oxygen partial pressure:
+    Pabar=H(cabar);
+    %
+    %mean alveolar oxygen partial pressure:
+    PAbar=RT.*cAbar;
+    %
+    %oxygen partial pressure in the inspired air:
+    PI=RT.*cI;
+    % 
+    PAbar_vec(i)=PAbar;
+    Pabar_vec(i)=Pabar;
+    Pv_vec(i)=Pv;
+    cAbar_vec(i)=cAbar;
+    cabar_vec(i)=cabar;
+end
 
 % Plot partial pressure of oxygen vs. cI
-Pressure = [PAbar;Pabar;Pv];
-plot(altitude,Pressure,'Linewidth',1.2);
-legend Aveolar Arterial Venous;
-title('Partial Pressure of Oxygen (Adapt to High Altitude)');
+Pressure = [PAbar_vec;Pabar_vec;Pv_vec];
+plot(beta,Pressure,'Linewidth',1.2);
+legend Alveolar Arterial Venous;
+title('Partial Pressure of Oxygen vs. Beta at 30,000ft');
 ylabel('Partial Pressure');
-xlabel('Altitude Above Sea Level (feet)');
+xlabel('Beta');
 
 % Plot concentration of oxygen vs. cI
-Concentration = [cAbar;cabar;cv];
-plot(altitude,Concentration,'Linewidth',1.2);
-legend Aveolar Arterial Venous;
-title('Concentration of Oxygen (Adapt to High Altitude)');
+Concentration = [cAbar_vec;cabar_vec;cv_vec];
+plot(beta,Concentration,'Linewidth',1.2);
+legend Alveolar Arterial Venous;
+title('Concentration of Oxygen vs. Beta at 30,000ft');
 ylabel('Concentration');
-xlabel('Altitude Above Sea Level (feet)');
+xlabel('Beta');
