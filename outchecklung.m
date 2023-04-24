4c6bcfbe5d921f3c8cb2b0dd62c2408e52f92fd4
celinelee115 <123041751+celinelee115@users.noreply.github.com>
2023-04-23 23:06:44 -0700

    outchecklung_task9.m

--- outchecklung.m
+++ outchecklung.m
@@ -4,69 +4,65 @@
 %solve for all other unknowns:
 %concentrations and partial pressures 
 %in the individual alveoli:
-for i=1:length(beta)
-    %vector of oxygen concentrations in 
-    %arterial blood leaving each alveolus:
-    ca=carterial(cv_vec(i),r(:,i));
-    %
-    %vector of oxygen partial pressures
-    %in arterial blood leaving each alveolus:
-    Pa=H(ca);
-    %
-    %vector of oxygen partial pressures 
-    %in alveolar air:
-    PA=Pa;
-    %
-    %vector of oxygen concentrations
-    %in alveolar air:
-    cA=PA/RT;
-    %
-    %concentrations and partial pressures 
-    %for the whole organism:
-    %(write out these results)
-    %oxygen concentration in (systemic) venous blood:
-    cv;
-    %
-    %mean arterial oxygen concentration:
-    cabar=Q' *ca/sum(Q);
-    %  
-    %mean alveolar oxygen concentration:
-    cAbar=VA'*cA/sum(VA);
-    %
-    %oxygen concentration is the inspired air:
-    cI;
-    %
-    %oxygen partial pressure in venous blood:
-    Pv=H(cv);
-    %
-    %mean arterial oxygen partial pressure:
-    Pabar=H(cabar);
-    %
-    %mean alveolar oxygen partial pressure:
-    PAbar=RT.*cAbar;
-    %
-    %oxygen partial pressure in the inspired air:
-    PI=RT.*cI;
-    % 
-    PAbar_vec(i)=PAbar;
-    Pabar_vec(i)=Pabar;
-    Pv_vec(i)=Pv;
-    cAbar_vec(i)=cAbar;
-    cabar_vec(i)=cabar;
-end
+
+%vector of oxygen concentrations in 
+%arterial blood leaving each alveolus:
+ca=carterial(cv,r);
+%
+%vector of oxygen partial pressures
+%in arterial blood leaving each alveolus:
+Pa=H(ca);
+%
+%vector of oxygen partial pressures 
+%in alveolar air:
+PA=Pa;
+%
+%vector of oxygen concentrations
+%in alveolar air:
+cA=PA/RT;
+%
+%concentrations and partial pressures 
+%for the whole organism:
+%(write out these results)
+%oxygen concentration in (systemic) venous blood:
+cv;
+%
+%mean arterial oxygen concentration:
+cabar=Q' *ca/sum(Q);
+%  
+%mean alveolar oxygen concentration:
+cAbar=VA'*cA/sum(VA);
+%
+%oxygen concentration is the inspired air:
+cI;
+%
+%oxygen partial pressure in venous blood:
+Pv=H(cv);
+%
+%mean arterial oxygen partial pressure:
+Pabar=H(cabar);
+%
+%mean alveolar oxygen partial pressure:
+PAbar=RT.*cAbar;
+%
+%oxygen partial pressure in the inspired air:
+PI=RT.*cI;
+% 
 
 % Plot partial pressure of oxygen vs. cI
-Pressure = [PAbar_vec;Pabar_vec;Pv_vec];
-plot(beta,Pressure,'Linewidth',1.2);
+Pressure = [PAbar;Pabar;Pv];
+plot(cstar,Pressure,'Linewidth',1.2);
 legend Alveolar Arterial Venous;
-title('Partial Pressure of Oxygen vs. Beta at 30,000ft');
+title('Partial Pressure of Oxygen vs. cstar');
 ylabel('Partial Pressure');
-xlabel('Beta');
+xlabel('cstar');
+xlim([min(cstar) max(cstar)]);
 
 % Plot concentration of oxygen vs. cI
-Concentration = [cAbar_vec;cabar_vec;cv_vec];
-plot(beta,Concentration,'Linewidth',1.2);
+Concentration = [cAbar;cabar;cv];
+plot(cstar,Concentration,'Linewidth',1.2);
 legend Alveolar Arterial Venous;
-title('Concentration of Oxygen vs. Beta at 30,000ft');
+title('Concentration of Oxygen vs. cstar');
 ylabel('Concentration');
-xlabel('Beta');
+xlabel('cstar');
+xlim([min(cstar) max(cstar)]);
