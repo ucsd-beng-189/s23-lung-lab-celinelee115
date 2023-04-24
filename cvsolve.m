b22d264d254000177d597352c12dc89ebd60ae50
celinelee115 <123041751+celinelee115@users.noreply.github.com>
2023-04-23 23:07:17 -0700

    cvsolve_task9.m

--- cvsolve.m
+++ cvsolve.m
@@ -3,24 +3,21 @@
 %   %stop program and issue error message:
 %   error('M is too large')
 % end
-for i=1:length(beta)
-    %initialize:
-    cv_lower=0;            %lower limit of interval
-    cv_upper=camax;        %upper limit of interval
-    cv=(cv_lower+cv_upper)/2; %midpoint of interval
+%initialize:
+cv_lower=0;            %lower limit of interval
+cv_upper=camax;        %upper limit of interval
+cv=(cv_lower+cv_upper)/2; %midpoint of interval
 
-    %begin main loop:
-    for count=1:maxcount
-      %check sign of Mdiff at midpoint of interval
-      pos=(Mdiff(cv,r(:,i))>0);
-      neg=~pos;
-      
-      %if(pos) cv_upper=cv; else cv_lower=cv:
-      cv_upper=cv_upper.*neg+cv.*pos; 
-      cv_lower=cv_lower.*pos+cv.*neg;
-      
-      %set cv=midpoint of current interval:
-      cv=(cv_lower+cv_upper)/2;
-    end
-    cv_vec(i)=cv;
+%begin main loop:
+for count=1:maxcount
+  %check sign of Mdiff at midpoint of interval
+  pos=(Mdiff(cv,r)>0);
+  neg=~pos;
+  
+  %if(pos) cv_upper=cv; else cv_lower=cv:
+  cv_upper=cv_upper.*neg+cv.*pos; 
+  cv_lower=cv_lower.*pos+cv.*neg;
+  
+  %set cv=midpoint of current interval:
+  cv=(cv_lower+cv_upper)/2;
 end
